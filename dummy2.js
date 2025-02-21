a7a510f757d786eab9ec60eb1432e93d1ad63402
ResitalCafe <bloodstoppermusic@gmail.com>
2025-02-21 23:24:35 +0300

    Create dummy2.js

---
+++ dummy2.js
@@ -0,0 +1,33 @@
+// dummy.js
+var menuItems = [
+  {
+    name: "Espresso",
+    price: "$2.50",
+    image: "images/espresso.jpg",
+    description: "Fincanda, sert ve enerji verici kafein deposu."
+  },
+  {
+    name: "Cappuccino",
+    price: "$3.50",
+    image: "images/cappuccino.jpg",
+    description: "Yumuşak espressonun sütlü köpükle mucizevi buluşması."
+  },
+  {
+    name: "Latte",
+    price: "$4.00",
+    image: "images/latte.jpg",
+    description: "Kremamsı dokusuyla, sıcak süt ve espressonun eseri."
+  },
+  {
+    name: "Kurabiye",
+    price: "$5.00",
+    image: "images/cookies.jpg",
+    description: "Taze ve sıcak, günlük pişirdiğimiz , fındıkların ve yulafın çikolatayla buluşması."
+  },
+  {
+    name: "Kruvasan",
+    price: "$2.00",
+    image: "images/croissant.jpg",
+    description: "Freshly baked, flaky and buttery pastry."
+  }
+];
