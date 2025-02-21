751f64278aa92787e001564229e16da0523880e0
ResitalCafe <bloodstoppermusic@gmail.com>
2025-02-21 12:10:46 +0300

    Update dummy.js

--- dummy.js
+++ dummy.js
@@ -1,1 +1,27 @@
-
+// dummy.js
+var menuItems = [
+  {
+    name: "Espresso",
+    price: "$2.50",
+    image: "images/espresso.jpg",
+    description: "A bold and energizing coffee shot."
+  },
+  {
+    name: "Cappuccino",
+    price: "$3.50",
+    image: "images/cappuccino.jpg",
+    description: "Smooth espresso with steamed milk foam."
+  },
+  {
+    name: "Latte",
+    price: "$4.00",
+    image: "images/latte.jpg",
+    description: "Creamy blend of espresso and steamed milk."
+  },
+  {
+    name: "Croissant",
+    price: "$2.00",
+    image: "images/croissant.jpg",
+    description: "Freshly baked, flaky and buttery pastry."
+  }
+];
